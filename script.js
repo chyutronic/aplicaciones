448937418a6ad6b3f6fde1581a9be2e3cca96210
chyutronic <chyutronic@gmail.com>
2022-07-22 20:33:46 -0400

    Se agrega ventana modal para mostrar los vinos (problema doble click)

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// código para usar la ventana modal
+
+const open = document.getElementById('open');
+const modal_container = document.getElementById('modal_container');
+const close = document.getElementById('close');
+
+
 // arreglo que contiene las respuestas con combinaciones posibles
 let op1 = ["1", "1", "1"];
 let op2 = ["2", "2", "2"];
@@ -22,28 +29,54 @@ function respuesta(numPregunta, seleccionada){
 function consultar(){
 
     if(JSON.stringify(op1) === JSON.stringify(opcionElegida)){
-        window.open("op1.html");
+        open.addEventListener('click', () => {
+            modal_container.classList.add('show');
+        });
     }
     else if(JSON.stringify(op2) === JSON.stringify(opcionElegida)){
         alert("Tu vino perfecto es GATO NEGRO...!");
+        location.reload();
+        window.scrollTo(0, 0);
     }
     else if(JSON.stringify(op3) === JSON.stringify(opcionElegida)){
         alert("Tu vino perfecto es GATO AZUL...!");
+        location.reload();
+        window.scrollTo(0, 0);
     }
     else if(JSON.stringify(op4) === JSON.stringify(opcionElegida)){
         alert("Tu vino perfecto es GATO ROJO...!");
+        location.reload();
+        window.scrollTo(0, 0);
     }
     else if(JSON.stringify(op5) === JSON.stringify(opcionElegida)){
         alert("Tu vino perfecto es GATO AMARILLO...!");
+        location.reload();
+        window.scrollTo(0, 0);
     }
     else{
         alert("Opción no reconocida. Vuelve a intentarlo..!")
+        location.reload();
+        window.scrollTo(0, 0);
     }
     
     // console.log(opcionElegida);
 
+    // location.reload();
+    // window.scrollTo(0, 0);
+    
+}// fin método consultar
+
+
+close.addEventListener('click', () => {
+    modal_container.classList.remove('show');
     location.reload();
     window.scrollTo(0, 0);
+});
 
-}// fin método consultar
+
+
+
+
+
+
 
