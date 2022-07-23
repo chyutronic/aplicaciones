f62e981f668fb06808a6d025a5b46db5b07c37c2
chyutronic <chyutronic@gmail.com>
2022-07-23 15:26:28 -0400

    Solución problema doble click e inclusión 6 alternativas de vino con ventana modal

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 // código para usar la ventana modal
 
 const open = document.getElementById('open');
-const modal_container = document.getElementById('modal_container');
-const close = document.getElementById('close');
+const modal_container1 = document.getElementById('modal_container1');
+const close1 = document.getElementById('close1');
+const modal_container2 = document.getElementById('modal_container2');
+const close2 = document.getElementById('close1');
+const modal_container3 = document.getElementById('modal_container3');
+const close3 = document.getElementById('close1');
+const modal_container4 = document.getElementById('modal_container4');
+const close4 = document.getElementById('close1');
+const modal_container5 = document.getElementById('modal_container5');
+const close5 = document.getElementById('close1');
+const modal_container6 = document.getElementById('modal_container6');
+const close6 = document.getElementById('close1');
+const modal_container28 = document.getElementById('modal_container28');
+const close28 = document.getElementById('close1');
 
 
 // arreglo que contiene las respuestas con combinaciones posibles
@@ -11,6 +23,7 @@ let op2 = ["2", "2", "2"];
 let op3 = ["3", "3", "3"];
 let op4 = ["1", "2", "3"];
 let op5 = ["3", "2", "1"];
+let op6 = ["1", "1", "2"];
 
 
 // arreglo donde se guardan las respuestas del usuario
@@ -30,48 +43,104 @@ function consultar(){
 
     if(JSON.stringify(op1) === JSON.stringify(opcionElegida)){
         open.addEventListener('click', () => {
-            modal_container.classList.add('show');
+            modal_container1.classList.add('show');
         });
     }
     else if(JSON.stringify(op2) === JSON.stringify(opcionElegida)){
-        alert("Tu vino perfecto es GATO NEGRO...!");
-        location.reload();
-        window.scrollTo(0, 0);
+        open.addEventListener('click', () => {
+            modal_container2.classList.add('show');
+        });
     }
     else if(JSON.stringify(op3) === JSON.stringify(opcionElegida)){
-        alert("Tu vino perfecto es GATO AZUL...!");
-        location.reload();
-        window.scrollTo(0, 0);
+        open.addEventListener('click', () => {
+            modal_container3.classList.add('show');
+        });
     }
     else if(JSON.stringify(op4) === JSON.stringify(opcionElegida)){
-        alert("Tu vino perfecto es GATO ROJO...!");
-        location.reload();
-        window.scrollTo(0, 0);
+        open.addEventListener('click', () => {
+            modal_container4.classList.add('show');
+        });
     }
     else if(JSON.stringify(op5) === JSON.stringify(opcionElegida)){
-        alert("Tu vino perfecto es GATO AMARILLO...!");
-        location.reload();
-        window.scrollTo(0, 0);
+        open.addEventListener('click', () => {
+            modal_container5.classList.add('show');
+        });
+    }
+    else if(JSON.stringify(op6) === JSON.stringify(opcionElegida)){
+        open.addEventListener('click', () => {
+            modal_container6.classList.add('show');
+        });
     }
     else{
-        alert("Opción no reconocida. Vuelve a intentarlo..!")
-        location.reload();
-        window.scrollTo(0, 0);
+        open.addEventListener('click', () => {
+            modal_container28.classList.add('show');
+        });
     }
-    
-    // console.log(opcionElegida);
-
-    // location.reload();
-    // window.scrollTo(0, 0);
     
 }// fin método consultar
 
 
-close.addEventListener('click', () => {
-    modal_container.classList.remove('show');
-    location.reload();
+// función para cerrar la ventana modal y reiniciar la aplicación
+function cerrarVentanaModal(){
+
     window.scrollTo(0, 0);
-});
+    location.reload();
+
+    // if(JSON.stringify(op1) === JSON.stringify(opcionElegida)){
+    //     close1.addEventListener('click', () => {
+    //         modal_container1.classList.remove('show');
+    //         location.reload();
+    //         window.scrollTo(0, 0);
+    //     });
+    // }
+    // else if(JSON.stringify(op2) === JSON.stringify(opcionElegida)){
+    //     close2.addEventListener('click', () => {
+    //         modal_container2.classList.remove('show');
+    //         location.reload();
+    //         window.scrollTo(0, 0);
+    //     });
+    // }
+    // else if(JSON.stringify(op3) === JSON.stringify(opcionElegida)){
+    //     close3.addEventListener('click', () => {
+    //         modal_container3.classList.remove('show');
+    //         location.reload();
+    //         window.scrollTo(0, 0);
+    //     });
+    // }
+    // else if(JSON.stringify(op4) === JSON.stringify(opcionElegida)){
+    //     close4.addEventListener('click', () => {
+    //         modal_container4.classList.remove('show');
+    //         location.reload();
+    //         window.scrollTo(0, 0);
+    //     });
+    // }
+    // else if(JSON.stringify(op5) === JSON.stringify(opcionElegida)){
+    //     close5.addEventListener('click', () => {
+    //         modal_container5.classList.remove('show');
+    //         location.reload();
+    //         window.scrollTo(0, 0);
+    //     });
+    // }
+    // else if(JSON.stringify(op6) === JSON.stringify(opcionElegida)){
+    //     close6.addEventListener('click', () => {
+    //         modal_container6.classList.remove('show');
+    //         location.reload();
+    //         window.scrollTo(0, 0);
+    //     });
+    // }
+    // else{
+    //     close28.addEventListener('click', () => {
+    //         modal_container28.classList.remove('show');
+    //         location.reload();
+    //         window.scrollTo(0, 0);
+    //     });
+    // }
+
+}// fin función cerrar
+
+
+
+
 
 
 
